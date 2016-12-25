9d870cf1fd942f885296ba36154642e85553057d
Dario Cangialosi <dario.cangialosi@gmail.com>
2016-12-25 18:15:20 +0100

    first attempt at non-blocking console input

--- udp_chat.py
+++ udp_chat.py
@@ -1,5 +1,50 @@
 #!/usr/bin/env python3
 
+'''
+http://stackoverflow.com/questions/2408560/python-nonblocking-console-input
+'''
+
+import sys
+import threading
+import queue
+
+def add_input(input_queue):
+	while True:
+		input_queue.put(sys.stdin.read(1))
+
+input_queue = queue.Queue()
+input_thread = threading.Thread(target=add_input, args=(input_queue,))
+input_thread.daemon = True
+input_thread.start()
+
+def getLine():
+	out=[]
+	while True:
+		if not input_queue.empty():
+			got=input_queue.get()
+			if got!='\n':
+				out.append(got)
+			else:
+				return ''.join(out)
+		else:
+			return None
+
+def testGetLine():
+	while True:
+		line = getLine()
+		if line is not None:
+			print('line', line)
+
+def bInput(prompt):
+	writef(prompt)
+	while True:
+		line = getLine()
+		if line is not None:
+			return line
+
+def writef(text):
+	sys.stdout.write(text)
+	sys.stdout.flush()
 
 '''
 from: https://thecodeninja.net/2014/12/udp-chat-in-python/
@@ -11,27 +56,15 @@ Here I will share a very basic UDP chat application in Python using sockets.
 It can work in point-to-point or broadcast mode.
 For Point-to-Point, enter IP and Port.
 For Broadcasting mode set the last byte of IP address to 255. i.e. 192.168.0.255.
-Port number is HEX, remove the base 16 to make it decimal.
 '''
 
 import socket
-import sys, select
- 
-# Read a line. Using select for non blocking reading of sys.stdin
-def getLine():
-	i,o,e = select.select([sys.stdin],[],[],0.0001)
-	for s in i:
-		if s == sys.stdin:
-			input = sys.stdin.readline()
-			return input
-	return False
- 
-ohost = input("Please Enter OUT-HOST-IP: ")
-if not ohost: ohost = '127.0.0.1'
-iport = int(input("Please Enter IN-PORT: "))
-oport = int(input("Please Enter OUT-PORT: "))
 
- 
+ohost = bInput("Please Enter OUT-HOST-IP: ")
+if not ohost: ohost = '127.0.0.1'
+iport = int(bInput("Please Enter IN-PORT: "))
+oport = int(bInput("Please Enter OUT-PORT: "))
+
 send_address = (ohost, oport) # Set the address to send to
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    # Create Datagram Socket (UDP)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Make Socket Reusable
@@ -39,8 +72,10 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Allow incoming broadca
 s.setblocking(False) # Set socket to non-blocking mode
 s.bind(('', iport)) #Accept Connections on port
 print("Accepting connections on port", iport)
- 
-while 1:
+
+import time
+
+while True:
 	try:
 		message, address = s.recvfrom(8192) # Buffer size is 8192. Change as needed.
 		if message:
@@ -48,8 +83,10 @@ while 1:
 	except BlockingIOError:
 		pass
  
-	input = getLine()
-	if(input != False):
-		data_to_send=input.rstrip('\n').encode()
-		print('sending',data_to_send,'to', send_address)
-		s.sendto(data_to_send, send_address)
+	line = getLine()
+	if(line != None):
+		data_to_send=line.rstrip('\n')
+		print(send_address,'<',data_to_send)
+		s.sendto(data_to_send.encode(), send_address)
+	else:
+		time.sleep(1)
